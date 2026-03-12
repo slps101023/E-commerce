2d5329ad21a6f04c0e2cb380372b9d44977fea0e
lihsun <slps101023@gmail.com>
2026-03-12 21:33:06 +0800

    chore:initial next.js framework

---
+++ my-next-admin/.next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Vo3I+rXe3SOwvljH21x0WdDHRwVIpovTHyBoFlAQXt8=\"\n}"
---
+++ my-next-admin/.next/dev/static/chunks/my-next-admin_src_app_layout_tsx_503d359e._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__67b269b1._.css"
+],
+    source: "dynamic"
+});
